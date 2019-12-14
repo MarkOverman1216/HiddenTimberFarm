ff49a8c3f11975c74b84465ca719dff5f41ca9eb
MarkOverman <markoverman1216@gmail.com>
2019-12-14 12:23:35 -0600

    Pulled Dev branch to avoid conflicts, got rid of examples

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,4 @@
 const Router = require("express").Router;
-const exampleRoutes = require("./examples");
-
-const apiRoutes = Router();
-
-apiRoutes.use("/examples", exampleRoutes);
 
 const dataFarm = Router();
 
