8b6f452898792eae193f77f5a36aa428eef6a3f2
Kevetic <kevetic@outlook.com>
2019-12-14 14:10:27 -0600

    hero-page animations done

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,1 +1,17 @@
 $("#home").hide();
+$("#scrollButton").hide();
+
+$("#nameButton").on("click", function() {
+  $("#scrollButton").fadeIn(5000);
+  $("#h1")
+    .fadeOut(1000)
+    .text("Hello ______ , Click below to continue")
+    .fadeIn(5000);
+  $("#input").hide();
+  $("#nameButton").hide();
+});
+
+$("#sliderButton").on("click", function() {
+  $("#home").fadeIn(25ç00);
+  $(".hero-full-screen").addClass("slideOutUp animated slower").fadeOut(1500);
+});
