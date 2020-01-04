0925f92d73c902f0bc412e3f949ef572d3dfd8ba
MarkOverman <markoverman1216@gmail.com>
2020-01-03 23:43:31 -0600

    First login test

---
+++ public/js/auth.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable camelcase */
+let auth0 = null;
+// ..
+const configureClient = async () => {
+  auth0 = await createAuth0Client({
+    domain: process.env.AUTH0Domain,
+    client_id: process.env.AUTH0clientId,
+    audience: process.env.AUDIENCE
+  });
+};
+// ..
+// ..
+window.onload = async () => {
+  await configureClient();
+  // NEW - update the UI state
+  // NEW - check for the code and state parameters
+  const query = window.location.search;
+  if (query.includes("code=") && query.includes("state=")) {
+    // Process the login state
+    await auth0.handleRedirectCallback();
+    // Use replaceState to redirect the user away and remove the querystring parameters
+    window.history.replaceState({}, document.title, "/");
+  }
+};
+
+const login = async () => {
+  await auth0.loginWithRedirect({
+    redirect_uri: window.location.origin
+  });
+};
+const logout = () => {
+  auth0.logout({
+    returnTo: window.location.origin
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,24 @@ const Router = require("express").Router;
 const apiRoutes = require("./apiRoutes");
 const htmlRoutes = require("./htmlRoutes");
 
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+
 const router = new Router();
 
-router.use("/api", apiRoutes);
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0Domain}/.well-known/jwks.json`
+  }),
+  audience: process.env.AUDIENCE,
+  issuer: `https://${process.env.AUTH0Domain}/`,
+  algorithm: ["RS256"]
+});
+
+router.use("/api", checkJwt, apiRoutes);
 router.use("", htmlRoutes);
 
 module.exports = router;
